de273d9d704a807ac45b60adfaa970d7768b4edd
yumehiro-omori <omori.y.mllab.nit@gmail.com>
2022-01-26 17:46:25 +0900

    arrange docs folder

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/si4ul/'))
+sys.path.insert(0, os.path.abspath('../si4ul/'))
 
 
 # -- Project information -----------------------------------------------------
